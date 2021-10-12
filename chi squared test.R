04cec9096d4edab7df221e2380476d8bad8710d1
AlexJRichter <91969557+AlexJRichter@users.noreply.github.com>
2021-10-12 12:06:16 -0500

    Add files via upload

--- chi squared test.R
+++ chi squared test.R
@@ -1,5 +1,5 @@
 chisquare.test = function(actual, expected)
 {
   chisq.statistic = sum((actual-expected)**2/expected)
-  return(pchisq(chisq.statistic, df=length(actual)-1, lower.tail=F))
+  return(pchisq(sum(chisq.statistic), df=length(actual)-1, lower.tail=F))
 }
